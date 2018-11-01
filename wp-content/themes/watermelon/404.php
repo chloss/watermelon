cb4f2829bb045eedd8065dbebe84cd8408c0ed97
Chloe Chua Hui Shan <chchu28@student.monash.edu>
2018-11-01 21:28:14 +1100

    chloe-style.css
    Ignore the error 404 page for now. Still playing around with it to figure out why the container is only that section even commenting out the grids.

--- wp-content/themes/watermelon/404.php
+++ wp-content/themes/watermelon/404.php
@@ -1,16 +1,11 @@
 <?php get_header(); ?>
-<div id="main-content">
-<div class="container">
-<div id="content-area" class="clearfix">
-<div id="left-area">
-<article id="post-0" <?php post_class( 'et_pb_post not_found' ); ?>>
-<img src="/image.jpg">
-<h1><?php esc_html_e('Page Not Found','WP Loop'); ?></h1>
-<p><?php esc_html_e('Oh No! The page you opened does not exist. Please use the links below to navigate to a safer place.', 'WP Loop'); ?></p>
-</article> <!-- .et_pb_post -->
-</div> <!-- #left-area -->
 
-</div> <!-- #content-area -->
-</div> <!-- .container -->
-</div> <!-- #main-content -->
+<div class="error">
+    <article id="post-0"<?php post_class( 'et_pb_post not_found' ); ?>>
+        <img src="404error.png"/>
+       <h1> <?php esc_html_e('Page Not Found','WP Loop'); ?> </h1>
+        <p> <?php esc_html_e('Oh No! The page you opened does not exist. Please use the links below to navigate to a safer place.', 'WP Loop'); ?></p>
+    </article> <!-- .et_pb_post -->
+</div>
+
 <?php get_footer(); ?>
