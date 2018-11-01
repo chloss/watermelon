93f2a1d021af353027640434e8475d05262a5b5d
Chloe Chua Hui Shan <chchu28@student.monash.edu>
2018-11-02 00:16:19 +1100

    Grid testing

---
+++ wp-content/themes/watermelon/index.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<div class="featuredevents">
+    <h1>test</h1>
+</div>
+
+
+<div class="about">
+    <h1>test</h1>
+</div>
+
+
+<div class="recentnews">
+    <h1>test</h1>
+</div>
+
+<div class="sponsors">
+    <h1>test</h1>
+</div>
+
+<div class="ads">
+    <h1>test</h1>
+</div>
+
+<?php get_footer(); ?>
